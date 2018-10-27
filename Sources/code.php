afeb6dfde3576b6c931b8979e773c53528343129
ducnguyenhuu31 <ducnguyenhuu31@gmail.com>
2018-10-28 01:27:44 +0700

    d

--- Sources/code.php
+++ Sources/code.php
@@ -1,23 +1,25 @@
-<?php include("include/header.php") ?>
-<div class="container">
-	<div class="row-fluid">
-		<div class="text-center"><h2><b>KHÔI PHỤC TÀI KHOẢN</b></h2></div>
-		<form method="post">
-			<?php recover_password(); ?>
-			<div class="form-group">
-				<p><b>Nhập mã code của bạn</b></p>
-            	<input type="text" class="form-control" name="" placeholder="**************">
-          	</div>
-			<dir class="row">
-				<div class="form-group">
-					<div class="span2">
-						<button type="submit" class="btn btn-primary" name="cancel-submit">Hủy bỏ</button>
+<?php include("include/header.php")?>
+<div class="background-color">
+	<div class="container">
+		<div class="row-fluid">
+			<div class="text-center"><h2 style="font-family: Arial"><b>KHÔI PHỤC TÀI KHOẢN</b></h2></div>
+			<form method="post">
+				<?php recover_password(); ?>
+				<div class="form-group" style="text-align: center;">
+					<p style="font-family: Arial; font-size: 20px"><b>Nhập mã code của bạn</b></p>
+	            	<input type="text" class="form-control" name="" placeholder="**************">
+	          	</div>
+				<dir class="row">
+					<div class="form-group" style="margin-left: 360px">
+						<div class="span2">
+							<button type="submit" class="btn btn-primary" name="cancel-submit">Hủy bỏ</button>
+						</div>
+						<div class="span2">
+							<button type="submit" class="btn btn-primary" name="recover-submit">Tiếp tục</button>
+						</div>
 					</div>
-					<div class="sapn2">
-						<button type="submit" class="btn btn-primary" name="recover-submit">Tiếp tục</button>
-					</div>
-				</div>
-			</dir>
-		</form>
+				</dir>
+			</form>
+		</div>
 	</div>
 </div>

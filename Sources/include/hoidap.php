bc6a9d9dd1e6311df7a4dcb64b2e099498aba0b8
ducnguyenhuu31 <ducnguyenhuu31@gmail.com>
2018-10-28 22:36:08 +0700

    duc

--- Sources/include/hoidap.php
+++ Sources/include/hoidap.php
@@ -1,7 +1,11 @@
 <div class="container">
 	<div class="form-group">
         <label for="exampleInputCity1">Viết câu hỏi của bạn</label>
-        <input type="text" class="form-control" id="exampleInputCity1" placeholder="địa chỉ">
+        <input type="text" class="form-control" id="exampleInputCity1" placeholder="câu hỏi của bạn">
     </div>
     <button type="submit" class="btn btn-success mr-2">Đăng</button>
+    <div class="text-center">
+    	<h4>Các câu hỏi thường gặp</h4>
+    	<label for="exampleInputCity1">....</label>
+    </div>
 </div>

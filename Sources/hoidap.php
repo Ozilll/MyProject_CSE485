506b59fade14f18c7a33d14adb0d3366e1f71b74
ducnguyenhuu31 <ducnguyenhuu31@gmail.com>
2018-10-28 22:04:02 +0700

    duc

---
+++ Sources/hoidap.php
@@ -0,0 +1,5 @@
+<?php include("include/header.php") ?>
+<?php include("include/nav-header.php") ?>
+<?php include("include/nav-header-menu.php") ?>
+<?php include("include/hoidap.php") ?>
+<?php include("include/footer.php") ?>
---
+++ Sources/include/hoidap.php
@@ -0,0 +1,7 @@
+<div class="container">
+	<div class="form-group">
+        <label for="exampleInputCity1">Viết câu hỏi của bạn</label>
+        <input type="text" class="form-control" id="exampleInputCity1" placeholder="địa chỉ">
+    </div>
+    <button type="submit" class="btn btn-success mr-2">Đăng</button>
+</div>

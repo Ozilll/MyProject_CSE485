e8eaec68573fd06374bb84804d4f43a523da32b3
Ozilll <ductruong199854@gmail.com>
2018-10-29 17:20:03 +0700

    Update 29/10/2018

--- Sources/include/all-project.php
+++ Sources/include/all-project.php
@@ -1,3 +1,8 @@
+<?php $sql = "SELECT topic, students.username_student, projects.content FROM join_project, lecturers, projects, students WHERE lecturers.id = join_project.id_lecturer AND students.id_student = join_project.id_student AND projects.id = join_project.id_project";
+	$result = query($sql);
+	confirm($result);
+ ?>
+
 <div class="homepage-projects">
 	<h4> TỔNG HỢP ĐỒ ÁN CỦA SINH VIÊN ĐÃ HOÀN THÀNH</h4>
 </div>
@@ -7,23 +12,19 @@
 	<thead>
 		<tr>
 			<th>Đề tài</th>
-			<th>Sinh viên</th>
+			<th>Tài khoản</th>
 			<th>Nội dung</th>
 			<th>Đánh giá</th>
 		</tr>
 	</thead>
 	<tbody id="myTable">
+		<?php while($row = fetch_array($result)): ?>
 		<tr>
-			<td>Quản lý đồ án tốt nghiệp</td>
-			<td>Trương Minh Đức</td>
-			<td>...</td>
+			<td><?php echo $row['topic']; ?></td>
+			<td><?php echo $row['username_student']; ?></td>
+			<td><?php echo $row['content']; ?></td>
 			<td>...</td>
 		</tr>
-		<tr>
-			<td>Arsenal</td>
-			<td>Ozil</td>
-			<td>...</td>
-			<td>...</td>
-		</tr>
+		<?php endwhile; ?>	
 	</tbody>
 </table>	
--- Sources/include/menu-homepage.php
+++ Sources/include/menu-homepage.php
@@ -29,36 +29,7 @@
 					</div>	
 				</div>
 				<div id="all-projects" class="tab_content">
-					<div class="homepage-projects">
-						<h4> TỔNG HỢP ĐỒ ÁN CỦA SINH VIÊN ĐÃ HOÀN THÀNH</h4>
-					</div>
-					<hr>
-					<input id="myInput" type="text" placeholder="Tìm kiếm..." style="margin-top: 20px;">
-					<hr>
-					<table class="table table-hover">
-						<thead>
-							<tr>
-								<th>Đề tài</th>
-								<th>Sinh viên</th>
-								<th>Nội dung</th>
-								<th>Đánh giá</th>
-							</tr>
-						</thead>
-						<tbody id="myTable">
-							<tr>
-								<td>Quản lý đồ án tốt nghiệp</td>
-								<td>Trương Minh Đức</td>
-								<td>...</td>
-								<td>...</td>
-							</tr>
-							<tr>
-								<td>Arsenal</td>
-								<td>Ozil</td>
-								<td>...</td>
-								<td>...</td>
-							</tr>
-						</tbody>
-					</table>	
+					<?php include("include/all-project.php") ?>	
 				</div>
 			</div>
 			

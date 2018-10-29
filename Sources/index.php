2cc8823b8a817eea252eef75bd8c3613f4357353
Ozilll <ductruong199854@gmail.com>
2018-10-29 14:54:52 +0700

    update

--- Sources/index.php
+++ Sources/index.php
@@ -1,11 +1,7 @@
 <?php include("include/header.php") ?>
 <?php include("include/nav-header.php") ?>
 <?php include("include/nav-header-menu.php") ?>
-<<<<<<< HEAD
 <?php include("include/all-projects.php") ?>
 <?php include("include/footer.php") ?>
-
-
-=======
 <?php include("include/menu-homepage.php") ?>
->>>>>>> a14909736be3a007171be399af334624fd4a727b
+
